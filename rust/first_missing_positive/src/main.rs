7f959654acd50290e8c8c6450cdbdc8ab48da63e
Mat-Teus <cley.mateus@gmail.com>
2026-05-15 14:42:36 -0300

    first missing positive using cyclic sort

--- rust/first_missing_positive/src/main.rs
+++ rust/first_missing_positive/src/main.rs
@@ -1,17 +1,22 @@
 fn main() {
-    let vec = vec![3,4,-1,1,2];
+    let vec = vec![1,2,3,4,5];
 
     println!("{}",first_missing_positive(vec));
 }
 
 pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
     let mut lesser = 1;
-
-    let mut vec_aux: Vec<_> = nums.iter().collect();
-    vec_aux.sort();
+    let mut nums = nums;
 
     for i in 0..nums.len(){
-        if vec_aux[i] == &lesser{
+        while nums[i] > 0 && nums[i] <= nums.len() as i32 && nums[i] != nums[(nums[i] - 1) as usize]{
+            let swap = nums[i] - 1;
+            nums.swap(i, swap as usize);
+        }
+    }
+
+    for i in 0..nums.len(){
+        if nums[i] == lesser{
             lesser+=1;
         }
     }   
