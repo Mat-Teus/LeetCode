bfcf005325f93896c5c1eac0600f6c2cb81500eb
Mat-Teus <cley.mateus@gmail.com>
2026-05-15 15:29:56 -0300

    Remove elements solved with two pointers

--- rust/remove_element/src/main.rs
+++ rust/remove_element/src/main.rs
@@ -1,23 +1,19 @@
 fn main() {
-    let mut vec = vec![3,2,2,3];
+    let mut vec = vec![3,2,2,3, 3];
     let target = 2;
 
     remove_element(&mut vec, target);
-
-    println!("{:?}", vec);
 }
 
 pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-    let mut k = 0;
+    let mut write = 0;
 
-    for i in 0..nums.len(){
-        if nums[i] == val{
-            nums[i] = i32::MAX;
-            k += 1;
+    for read in 0..nums.len(){
+        if nums[read] != val{
+            nums[write] = nums[read];
+            write += 1;
         }
     }
 
-    nums.sort();
-
-    return nums.len() as i32 - k;
+    return write as i32;
 }
