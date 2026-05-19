05963cb59427a5132b906e3740b0baf6f5fdd9b5
Mat-Teus <cley.mateus@gmail.com>
2026-05-19 20:37:55 -0300

    Good array solution with counting sort

--- rust/check_if_a_array_is_good/src/main.rs
+++ rust/check_if_a_array_is_good/src/main.rs
@@ -1,25 +1,29 @@
 fn main() {
-    let vec = vec![1, 8];
+    let vec = vec![1,1];
 
 println!("{}", is_good(vec));
 }
 
 pub fn is_good(nums: Vec<i32>) -> bool {
-    let mut count = 0;
-    let mut vec = nums;
-    vec.sort();
+    let max = *nums.iter().max().unwrap();
+    let mut count = vec![0; (max + 1) as usize];
 
-    for i in 0..vec.len(){
-        if i+1 == vec[i] as usize{
-            count+=1;
+    for i in nums.iter() {
+        count[*i as usize] += 1;
+    }
+
+    let mut i = 1;
+
+    while i < count.len() - 2{
+        if count[i] == 0 || count[i] > 1{
+            return false;
         }
+        i+=1;
     }
 
-    if vec.len() == 1{
-        return false;
-    }else if count == vec.len() - 1 && *vec.last().unwrap() == vec[vec.len() - 2]{
+    if count[count.len() - 1] == 2{
         return true;
+    }else{
+        return false;
     }
-
-   return false;     
 }
