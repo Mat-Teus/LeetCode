fb8bb285df872fea0383db84e97efda10e2b0c3b
Mat-Teus <cley.mateus@gmail.com>
2026-05-23 10:36:15 -0300

    Refactored 3Sum Closest solution to improve readability and remove unnecessary checks.

--- rust/three_sum_closest/src/main.rs
+++ rust/three_sum_closest/src/main.rs
@@ -10,37 +10,15 @@ pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
     nums.sort();
 
     let mut ptr_still = 0;
-    let mut ptr_one:i32 = 0;
+    let mut ptr_one:i32 = ptr_still + 1;
     let mut ptr_two:i32 = (nums.len() - 1) as i32;
     let mut closest = nums[0] + nums[1] + nums[2];
 
-    while ptr_still < nums.len(){
+    while ptr_still < (nums.len() - 2) as i32{
         while ptr_one < ptr_two{
-            if ptr_one == ptr_still as i32{
-                ptr_one+=1;
-            }
-
-            if ptr_two == ptr_still as i32{
-                ptr_two -= 1;
-            }
-
-            if ptr_one == ptr_two{
-                break;
-            }
-
-            if ptr_one >= nums.len() as i32{
-                break;
-            }
-
-            if ptr_two < 0{
-                break;
-            }
-
-            let aux = nums[ptr_still] + nums[ptr_one as usize] + nums[ptr_two as usize];
-            if aux == target{
-                closest = aux;
-                return closest;
-            }else if (aux - target).abs() <= (closest - target).abs(){
+            let aux = nums[ptr_still as usize] + nums[ptr_one as usize] + nums[ptr_two as usize];
+    
+            if (nums[ptr_still as usize] + nums[ptr_one as usize] + nums[ptr_two as usize] - target).abs() < (closest - target).abs(){
                 closest = aux;
             }
             
@@ -48,10 +26,12 @@ pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
                 ptr_one+=1;
             }else if aux > target{
                 ptr_two-=1;
+            }else{
+                return target;
             }
         }
         ptr_still+=1;
-        ptr_one = 0;
+        ptr_one = ptr_still + 1;
         ptr_two = (nums.len() - 1) as i32;
     }
 
