8c11d01b37773970c60fa4f9c76d1319b9f745b3
Mat-Teus <cley.mateus@gmail.com>
2026-05-24 20:41:15 -0300

    3sum optimization

--- rust/three_sum/src/main.rs
+++ rust/three_sum/src/main.rs
@@ -9,32 +9,26 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
     nums.sort();
     let mut solution: Vec<Vec<i32>> = Vec::new();
     let mut ptr_still = 0;
-    let mut ptr_one = 0;
+    let mut ptr_one = ptr_still as i32 + 1;
     let mut ptr_two:i32 = (nums.len() - 1) as i32;
 
-    while ptr_still < nums.len(){
+    while ptr_still < nums.len() - 2{
         while ptr_one < ptr_two{
-            if ptr_two < 0 || ptr_one >= nums.len() as i32{
-                break;
-            }
+            let sum = nums[ptr_still] + nums[ptr_one as usize] + nums[ptr_two as usize];
 
-            if nums[ptr_still] + nums[ptr_one as usize] + nums[ptr_two as usize] == 0{
+            if sum == 0{
                 solution.push(vec![nums[ptr_still], nums[ptr_one as usize], nums[ptr_two as usize]]);
                 ptr_one+=1;
                 ptr_two-=1;
-            }else if nums[ptr_still] + nums[ptr_one as usize] + nums[ptr_two as usize] > 0{
+            }else if sum > 0{
                 ptr_two -= 1;
-            }else if nums[ptr_still] + nums[ptr_one as usize] + nums[ptr_two as usize] < 0{
+            }else if sum < 0{
                 ptr_one+=1;
             }
         }
         ptr_still+=1;
         ptr_one = ptr_still as i32 + 1;
         ptr_two = (nums.len() - 1) as i32;
-    }
-
-    for repeat in &mut solution {
-        repeat.sort();
     }
 
     solution.sort();
