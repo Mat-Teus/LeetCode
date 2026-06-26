11c332210fe001f8af24903b4adbc20566b02cbd
Mat-Teus <cley.mateus@gmail.com>
2026-06-26 14:54:16 -0300

    sorted squares with two pointers

--- rust/squares_of_a_sorted_array/src/main.rs
+++ rust/squares_of_a_sorted_array/src/main.rs
@@ -1,17 +1,28 @@
 fn main() {
-    let vec = vec![-4,-1,0,3,10];
+    let vec = vec![1,2,3,4,5];
 
     println!("{:?}", sorted_squares(vec));
 }
 
 pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
-    let mut solution = Vec::new();
+    let mut solution = vec![0;nums.len()];
+    let mut ptr_one = 0;
+    let mut ptr_two:usize = nums.len() - 1;
+    let mut pos = nums.len() - 1;
 
-    for i in nums{
-        solution.push(i.pow(2));
+    while pos > 0{
+        if nums[ptr_one].abs() > nums[ptr_two].abs(){
+            solution[pos] = nums[ptr_one].pow(2);
+            ptr_one+=1;
+        }else{
+            solution[pos] = nums[ptr_two].pow(2);
+            ptr_two-=1;
+        }
+
+        pos -= 1;
     }
 
-    solution.sort();
+    solution[0] = nums[ptr_one].pow(2);
 
     return solution;
 }
