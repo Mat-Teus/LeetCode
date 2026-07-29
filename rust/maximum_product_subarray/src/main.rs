9de09e4647692bbc3039ba3455f9772b9f56a002
Mat-Teus <cley.mateus@gmail.com>
2026-07-29 09:46:54 -0300

    maximum product subarray solution

---
+++ rust/maximum_product_subarray/src/main.rs
@@ -0,0 +1,25 @@
+use std::cmp::{max, min};
+
+fn main() {
+    let vec = vec![-2,-3,7];
+
+    println!("{}", max_product(vec));
+}
+
+pub fn max_product(nums: Vec<i32>) -> i32 {
+    let mut max_value = nums[0];
+    let mut min_value = nums[0];
+    let mut answer = nums[0];
+
+    for i in 1..nums.len(){
+        let prev = max_value;
+        let prev_min = min_value;
+
+        max_value = max(nums[i], max(nums[i] * prev_min, nums[i] * prev));
+        min_value = min(nums[i], max(nums[i] * prev, nums[i] * prev_min));
+        
+        answer = max(answer, max_value);
+    }
+
+    return answer;        
+}
