e324dc7a9b3fcd10ead3e1120db67b6045e4ff6e
Mat-Teus <cley.mateus@gmail.com>
2026-07-31 09:39:08 -0300

    minimum number of pushes to tyoe words 2 solution

---
+++ rust/minimum_number_of_pushes_to_type_word_2/src/main.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+fn main() {
+    let s = String::from("abcdef");
+
+    println!("{}", minimum_pushes(s));
+}
+
+pub fn minimum_pushes(word: String) -> i32 {
+    let mut hash = HashMap::new();
+    let mut count: i32 = 0;
+
+
+    for i in word.chars(){
+        *hash.entry(i).or_insert(0) += 1;
+    }
+
+    let mut freq:Vec<i32> = hash.values().copied().collect();
+
+    freq.sort();
+    freq.reverse();
+
+    let mut i = 0;
+
+    while i < freq.len(){
+        if i < 8{
+            while freq[i] > 0{
+                freq[i]-=1;
+                count+=1;
+            }
+        }else if i >= 8 && i < 16{
+            while freq[i] > 0{
+                freq[i]-=1;
+                count+=2;
+            }
+        }else if i >= 16 && i < 24{
+            while freq[i] > 0{
+                freq[i]-=1;
+                count+=3;
+            }
+        }else if i >= 24{
+            while freq[i] > 0{
+                freq[i]-=1;
+                count+=4;
+            }
+        }
+
+        i+=1;
+    }
+
+    return count;
+}
